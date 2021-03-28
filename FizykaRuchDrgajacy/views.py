d1238e2da5ce1547e807a8f952c10b3c3e3c6387
alekszych <aleks.zych@icloud.com>
2021-03-28 22:36:29 +0200

    added data verification

--- FizykaRuchDrgajacy/views.py
+++ FizykaRuchDrgajacy/views.py
@@ -6,6 +6,7 @@ import shutil
 
 apiIp = "http://83.11.31.190:25565/"
 
+
 def home(request):
     return render(request, 'home_page.html', {
 
@@ -17,20 +18,24 @@ def vChart(request):
         if ("amplituda" in request.POST
                 and "okres_drgan" in request.POST
                 and "faza" in request.POST):
-            response = str(apiIp+"wykres_v?"+"amp="+request.POST["amplituda"]+"&"+"okres="+request.POST["okres_drgan"]+"&"+"faza="+request.POST["faza"])
-            return render(request, 'vChart_page.html', {
-                "chart": response,
-            })
+            if float(request.POST["amplituda"]) > 0 and float(request.POST["faza"]) > 0 and float(request.POST["okres_drgan"]) > 0:
+                response = str(apiIp + "wykres_v?" + "amp=" + request.POST["amplituda"] + "&" + "okres=" + request.POST[
+                    "okres_drgan"] + "&" + "faza=" + request.POST["faza"])
+                return render(request, 'vChart_page.html', {
+                    "chart": response,
+                })
     return render(request, 'vChart_page.html', {})
 
 
 def xChart(request):
     if request.POST is not None:
         if ("amplituda" in request.POST and "okres_drgan" in request.POST):
-            response = str(apiIp+"wykres_x?"+"amp="+request.POST["amplituda"]+"&"+"okres="+request.POST["okres_drgan"])
-            return render(request, 'xChart_page.html', {
-                "chart": response,
-            })
+            if float(request.POST["amplituda"]) > 0 and float(request.POST["okres_drgan"]) > 0:
+                response = str(
+                    apiIp + "wykres_x?" + "amp=" + request.POST["amplituda"] + "&" + "okres=" + request.POST["okres_drgan"])
+                return render(request, 'xChart_page.html', {
+                    "chart": response,
+                })
     return render(request, 'xChart_page.html', {})
 
 
@@ -39,10 +44,12 @@ def aChart(request):
         if ("amplituda" in request.POST
                 and "okres_drgan" in request.POST
                 and "faza" in request.POST):
-            response = str(apiIp+"wykres_a?"+"amp="+request.POST["amplituda"]+"&"+"okres="+request.POST["okres_drgan"]+"&"+"faza="+request.POST["faza"])
-            return render(request, 'aChart_page.html', {
-                "chart": response,
-            })
+            if float(request.POST["amplituda"]) > 0 and float(request.POST["okres_drgan"]) > 0 and float(request.POST["faza"]) > 0:
+                response = str(apiIp + "wykres_a?" + "amp=" + request.POST["amplituda"] + "&" + "okres=" + request.POST[
+                    "okres_drgan"] + "&" + "faza=" + request.POST["faza"])
+                return render(request, 'aChart_page.html', {
+                    "chart": response,
+                })
     return render(request, 'aChart_page.html', {})
 
 
@@ -52,7 +59,10 @@ def tValues(request):
                 and "okres_drgan" in request.POST
                 and "faza" in request.POST
                 and "czas" in request.POST):
-                response = requests.get(apiIp+"wartosci_t?"+"amp="+request.POST["amplituda"]+"&"+"okres="+request.POST["okres_drgan"]+"&"+"faza="+request.POST["faza"]+"&"+"czas="+request.POST["czas"])
+            if float(request.POST["amplituda"]) > 0 and float(request.POST["okres_drgan"]) > 0 and float(request.POST["czas"]) > 0:
+                response = requests.get(
+                    apiIp + "wartosci_t?" + "amp=" + request.POST["amplituda"] + "&" + "okres=" + request.POST[
+                        "okres_drgan"] + "&" + "faza=" + request.POST["faza"] + "&" + "czas=" + request.POST["czas"])
                 if response.status_code == 200:
                     return render(request, 'tValues_page.html', {
                         "x": response.json()["x(t)"],
@@ -67,9 +77,12 @@ def tValues(request):
 def pendulum(request):
     if request.POST is not None:
         if "amplituda" in request.POST and "okres_drgan" in request.POST:
-            response = str(apiIp + "wahadlo?" + "amp=" + request.POST["amplituda"] + "&" + "okres=" + request.POST["okres_drgan"])
-            print(str(apiIp + "wahadlo?" + "amp=" + request.POST["amplituda"] + "&" + "okres=" + request.POST["okres_drgan"]))
-            return render(request, 'pendulum_page.html', {
-                "gif": response,
-            })
+            if float(request.POST["amplituda"]) > 0 and float(request.POST["okres_drgan"]) > 0:
+                response = str(apiIp + "wahadlo?" + "amp=" + request.POST["amplituda"] + "&" + "okres=" + request.POST[
+                    "okres_drgan"])
+                print(str(apiIp + "wahadlo?" + "amp=" + request.POST["amplituda"] + "&" + "okres=" + request.POST[
+                    "okres_drgan"]))
+                return render(request, 'pendulum_page.html', {
+                    "gif": response,
+                })
     return render(request, 'pendulum_page.html', {})
